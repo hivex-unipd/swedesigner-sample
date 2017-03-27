ec8534bf4cf426a9d6489e3ced365659e98cd5df
BaraccoPaolo <paolo.baracco.1@studenti.unipd.it>
2017-03-27 13:01:44 +0200

    finita struttura resources

--- src/main/java/server/template/java/JavaTemplate.java
+++ src/main/java/server/template/java/JavaTemplate.java
@@ -40,5 +40,47 @@ public class JavaTemplate extends Template {
 		STGroup group = new STGroupDir("src/main/resources/ifjavatemplate");
 		ST st = group.getInstanceOf("ifjavatemplate");// TODO Auto-generated method stub
 		return st;
-	}	
+	}
+	
+	@Override
+	public ST getCustomTemplate() {
+		STGroup group = new STGroupDir("src/main/resources/customjavatemplate");
+		ST st = group.getInstanceOf("customjavatemplate");// TODO Auto-generated method stub
+		return st;
+	}
+	
+	@Override
+	public ST getReturnTemplate() {
+		STGroup group = new STGroupDir("src/main/resources/returnjavatemplate");
+		ST st = group.getInstanceOf("returnjavatemplate");// TODO Auto-generated method stub
+		return st;
+	}
+	
+	@Override
+	public ST getAssignmentTemplate() {
+		STGroup group = new STGroupDir("src/main/resources/assignmentjavatemplate");
+		ST st = group.getInstanceOf("assignmentjavatemplate");// TODO Auto-generated method stub
+		return st;
+	}
+	
+	@Override
+	public ST getInitializationTemplate() {
+		STGroup group = new STGroupDir("src/main/resources/initializationjavatemplate");
+		ST st = group.getInstanceOf("initializationjavatemplate");// TODO Auto-generated method stub
+		return st;
+	}
+	
+	@Override
+	public ST getForTemplate() {
+		STGroup group = new STGroupDir("src/main/resources/forjavatemplate");
+		ST st = group.getInstanceOf("forjavatemplate");// TODO Auto-generated method stub
+		return st;
+	}
+	
+	@Override
+	public ST getWhileTemplate() {
+		STGroup group = new STGroupDir("src/main/resources/whilejavatemplate");
+		ST st = group.getInstanceOf("whilejavatemplate");// TODO Auto-generated method stub
+		return st;
+	}
 }
